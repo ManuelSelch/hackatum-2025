bf81feb1a4ceb70e0715b66589bbc8c4f15cebcc
Manuel Selch <manuel@selch.de>
2025-11-22 15:49:14 +0100

    debug(login): add prints

--- shared/src/commonMain/kotlin/login/LoginAPI.kt
+++ shared/src/commonMain/kotlin/login/LoginAPI.kt
@@ -19,24 +19,24 @@ class LoginAPI {
     private val client = HttpClient()
 
     suspend fun login(username: String, password: String): Result<LoginResponse> {
-        print("login")
         return post("/auth/login", LoginRequest(username, password))
     }
 
     suspend fun register(username: String, password: String): Result<UserResponse> {
-        print("register")
         return post("/auth/register", RegisterRequest(username, username, password))
     }
 
     private suspend inline fun <reified I, reified O> post(endpoint: String, request: I): Result<O> {
-        print("POST $endpoint")
+        println("POST $endpoint")
         return try {
             val response: O = client.post(URL + endpoint) {
                 contentType(ContentType.Application.Json)
                 setBody(request)
             }.body()
+            println("success: $response")
             Result.success(response)
         } catch (e: Exception) {
+            println("error: $e")
             Result.failure(e)
         }
     }
